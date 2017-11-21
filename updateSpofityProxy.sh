8e0325d10473eef08fd010004490675a1f7d0f8a
Oren Samuel <orens@mac-il-orens.local>
2017-11-21 16:42:49 +0200

    fix script for mac

--- updateSpofityProxy.sh
+++ updateSpofityProxy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 LINUX_SPOTIFY_CONF="$HOME/.var/app/com.spotify.Client/config/spotify/prefs"
-MACOS_SPOTIFY_CONF="$HOME/Library/Application\ Support/Spotify/prefs"
+MACOS_SPOTIFY_CONF="$HOME/Library/Application Support/Spotify/prefs"
 
 if [ -e $LINUX_SPOTIFY_CONF ]; then
 	SPOTIFY_CONF=$LINUX_SPOTIFY_CONF
-elif [ -e $MACOS_SPOTIFY_CONF ]; then
+elif [ -e "$MACOS_SPOTIFY_CONF" ]; then
 	SPOTIFY_CONF=$MACOS_SPOTIFY_CONF
 else
-	echo "spotify prefs not found"
+	echo "spotify prefs not found: $MACOS_SPOTIFY_CONF"
 	exit 1
 fi
 
@@ -19,17 +19,17 @@ if [ $ret_code != 0 ]; then
       echo "Error when executing command: 'curl'"
       exit $ret_code
 elif [[ $PROXY =~ ([0-9]{1,3}\.){3}([0-9]{1,3}):[0-9]{1,4} ]]; then
-	if grep -q 'network.proxy.addr="' $SPOTIFY_CONF; then 
+	if grep -q 'network.proxy.addr="' "$SPOTIFY_CONF"; then 
 		echo "updating proxy to $PROXY"
-		sed -i.bck -E 's/(network.proxy.addr=")[^"]+"/\1'$PROXY'@'$PROXY_TYPE'"/' $SPOTIFY_CONF
+		sed -i.bck -E 's/(network.proxy.addr=")[^"]+"/\1'$PROXY'@'$PROXY_TYPE'"/' "$SPOTIFY_CONF"
 	else
 		echo "adding proxy settings $PROXY"
-		if grep -q 'network.proxy.mode=' $SPOTIFY_CONF; then
-			sed -i.bck -E 's/(network.proxy.mode=)[0-9]+/\12/' $SPOTIFY_CONF
+		if grep -q 'network.proxy.mode=' "$SPOTIFY_CONF"; then
+			sed -i.bck -E 's/(network.proxy.mode=)[0-9]+/\12/' "$SPOTIFY_CONF"
 		else
-			echo -e "network.proxy.mode=2\n" >> $SPOTIFY_CONF
+			echo -e "network.proxy.mode=2\n" >> "$SPOTIFY_CONF"
 		fi
-		echo -e 'network.proxy.addr="'$PROXY'@'$PROXY_TYPE'"\n' >> $SPOTIFY_CONF
+		echo -e 'network.proxy.addr="'$PROXY'@'$PROXY_TYPE'"\n' >> "$SPOTIFY_CONF"
 	fi
 else
 	echo "bad proxy reply $PROXY"
